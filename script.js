771a26d3e664e22ec7c860f80cdcfc34e15b1d2f
Confus1on1212 <confus1oncsgo@gmail.com>
2025-02-13 11:01:17 +0100

    fetch

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 window.addEventListener('DOMContentLoaded', fetchCharacters());
 
 async function fetchCharacters() {
-    
+    try {
+        const response = await fetch("https://raw.githubusercontent.com/Laboratoria/LIM011-data-lovers/master/src/data/potter/potter.json");
+    } catch (error) {
+        
+    }
 }
