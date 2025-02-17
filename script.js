50921ef1a32bead4cc83841d9f9973e2a553d59e
Confus1on1212 <confus1oncsgo@gmail.com>
2025-02-17 13:38:55 +0100

    displayCharacter()

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ async function fetchCharacters() {
     try {
         const response = await fetch("https://raw.githubusercontent.com/Laboratoria/LIM011-data-lovers/master/src/data/potter/potter.json");
         const characters = await response.json();
-        console.log(characters);
+        displayCharacters(characters);
     } catch (error) {
         console.log(error)
     }
+}
+
+function displayCharacters(characters) {
+    
 }
